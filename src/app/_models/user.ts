eef9eb89afb24b5ff7a81d3f72d2fdeb59e13456
UVSManaswini <uvsmanaswini@gmail.com>
2021-05-16 15:24:40 +0530

    second

---
+++ src/app/_models/user.ts
@@ -0,0 +1,12 @@
+﻿export class User {
+    id: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    country: string;
+    state: string;
+    city: string;
+    token: string;
+}
